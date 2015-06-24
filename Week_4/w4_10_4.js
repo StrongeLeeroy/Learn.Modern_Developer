48835e1872ac93e521cdc08778a62cce46b5854f
StrongeLeeroy <gorka91@gmail.com>
2015-06-24 09:17:21 +0200

    Update 10.4.

--- Week_4/w4_10_4.js
+++ Week_4/w4_10_4.js
@@ -2,13 +2,15 @@ function letterChanges(string) {
 	var vowelMatch = /[aeiou]/;
 	var letterMatch = /[a-zA-Z]/;
 	var currentCharCode = 0;
-	string = string.split('');
+	string = string.toLowerCase().split('');
 
 	return string.map(function(letter){
 	  	if (letterMatch.test(letter)) {
-	  		console.log(letter + ' is a letter.');
 	    	currentCharCode = letter.charCodeAt(0);
 			currentCharCode++;
+			if (currentCharCode === 123) {
+				currentCharCode = 97;
+			};
 			letter = String.fromCharCode(currentCharCode);
 	    }
 	    if (vowelMatch.test(letter)) {
